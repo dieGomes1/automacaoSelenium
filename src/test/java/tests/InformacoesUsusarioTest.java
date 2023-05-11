677fa71d1974afc3cc7482e6e06def739f70e07c
diegomes <R@fael2906>
2023-05-10 21:46:47 -0300

    atualização validação de login na pagina

--- src/test/java/tests/InformacoesUsusarioTest.java
+++ src/test/java/tests/InformacoesUsusarioTest.java
@@ -1,16 +1,12 @@
 package tests;
 
-import static org.junit.Assert.*;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+import static org.junit.Assert.assertEquals;
 
 /**
  * site: <a href="http://www.juliodelima.com.br/taskit">...</a>
