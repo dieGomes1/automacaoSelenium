e35b9f89e70a13b01716865307bbdcc00d81404e
diegomes <R@fael2906>
2023-05-10 21:45:46 -0300

    atualização validação de login na pagina

--- src/test/java/tests/InformacoesUsusarioTest.java
+++ src/test/java/tests/InformacoesUsusarioTest.java
@@ -1,10 +1,16 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 /**
  * site: http://www.juliodelima.com.br/taskit
@@ -13,20 +19,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 
 public class InformacoesUsusarioTest {
+
+    String usuario = "diegomes2023";
+    String senha = "diegomes2023";
+
     @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
         WebDriver navegador = new ChromeDriver();
 
         navegador.get("http://www.juliodelima.com.br/taskit");  //=> acessa a pagina de login
-        navegador.findElement(By.linkText("Sign in")).click();//=> clicar no texto "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();  //=> clicar no texto "Sign in"
+        navegador.findElement(By.id("login-sign-in")).click();  //=> clicar no campo com o id:"login-sign-in"
+        navegador.findElement(By.id("login-sign-in")).sendKeys(usuario);   //=> preencher o campo com o usuario
+        navegador.findElement(By.id("password-sign-in")).click();  //=> clicar no campo com o id:"password-sign-in"
+        navegador.findElement(By.id("password-sign-in")).sendKeys(senha);   //=> preencher o campo com a senha
+        navegador.findElement(By.id("btn-submit-sign-in")).click();   //=> clicar no link que tenha o id:"btn-submit-sign-in"
 
-//        clicar no campo com o id:"login-sign-in"
-//        preencher o campo com o usuario
-//        clicar no campo com o id:"password-sign-in"
-//        preencher o campo com a senha
-//        clicar no link que tenha o id:"btn-submit-sign-in"
-//        valida que o usuario esta logado atraves da class="me"
+        Thread.sleep(2000);
+        WebElement me = navegador.findElement(By.className("me"));
+        String texto = me.getText();
 
+        assertEquals("Hi, diego",texto);
+
+        navegador.close();
     }
 }
